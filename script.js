07ffc820be301f3ab247fe168502939cbd0182f6
Sumit Desale <sumitdesale706@gmail.com>
2026-09-22 02:23:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	 constructor(width, height){
+		 this.height = height;
+		 this.width = width;
+	 }
 
-class Square extends Animal {}
+	 getWidth(){
+		 return this.width;
+	 }
+
+     getHeight(){
+		 return this.height;
+	 }
+
+     getArea(){
+		 return (this.width * this.height)
+	 }
+}
+
+class Square extends Rectangle {
+	 constructor(side){
+		 this.width = super.getWidth;
+		 this.getHeight = super.getHeight;
+		 this.side = side;
+	 }
+
+	 getPerimeter(){
+		 return (4 * this.side);
+	 }
+	 
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
